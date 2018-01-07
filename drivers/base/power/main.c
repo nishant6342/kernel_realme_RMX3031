60580f52959853340148fa9f110c6f3300b4d609
andip71 <andreasp@gmx.de>
2018-01-08 00:50:49 +0100

    power: Add generic wakelock blocker driver v1.1.0
    Based on ideas of FranciscoFranco's non-generic driver.
    
    Sysfs node:
    
    	/sys/class/misc/boeffla_wakelock_blocker/wakelock_blocker
    
    		- list of wakelocks to be blocked, separated by semicolons
    
    	/sys/class/misc/boeffla_wakelock_blocker/debug
    
    		- write: 0/1 to switch off and on debug logging into dmesg
    		- read:  get current driver internals
    
    	/sys/class/misc/boeffla_wakelock_blocker/version
    
    		- show driver version
    
    Signed-off-by: andip71 <andreasp@gmx.de>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>
    Signed-off-by: Shwetank Rai <shwetankrai12@gmail.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -40,6 +40,14 @@
 #include <mt-plat/mboot_params.h>
 #endif
 
+#ifdef CONFIG_SEC_DEBUG
+#include <linux/sec_debug.h>
+#endif
+
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER
+void pm_print_active_wakeup_sources(void);
+#endif
+
 typedef int (*pm_callback_t)(struct device *);
 
 /*
@@ -926,6 +934,9 @@ void dpm_resume_early(pm_message_t state)
 	ktime_t starttime = ktime_get();
 
 	trace_suspend_resume(TPS("dpm_resume_early"), state.event, true);
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER
+	pm_print_active_wakeup_sources();
+#endif
 	mutex_lock(&dpm_list_mtx);
 	pm_transition = state;
 
