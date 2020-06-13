eebf8fc7f83e0e5862f3f366106841ae2fe6cef5
Denis Efremov <efremov@linux.com>
2020-06-13 11:02:24 +0300

    power: wl_blocker: add generic size
    Signed-off-by: Denis Efremov <efremov@linux.com>
    Signed-off-by: acuicultor <ramoncuenca4@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: Little-W <1405481963@qq.com>
    Signed-off-by: Shwetank Rai <shwetankrai12@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -19,5 +19,5 @@
 #define LIST_WL_DEFAULT			"wlan_wake;wlan_rx_wake;wlan_ctrl_wake;wlan_txfl_wake;bluetooth_timer;BT_bt_wake;BT_host_wake;bbd_wake_lock;ssp_sensorhub_wake_lock;ssp_wake_lock;ssp_comm_wake_lock;mmc0_detect;grip_wake_lock;wlan_scan_wake;wlan_pm_wake;nfc_wake_lock"
 
 #define LENGTH_LIST_WL				1024
-#define LENGTH_LIST_WL_DEFAULT		(sizeof(LIST_WL_DEFAULT))
+#define LENGTH_LIST_WL_DEFAULT		(strlen(LIST_WL_DEFAULT) + 1)
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
