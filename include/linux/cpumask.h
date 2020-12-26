bb32bcb66f88beee00ce8750b0e094b362eff4f3
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-26 00:46:31 -0800

    cpumask: Add cpumasks for big, LITTLE, and prime CPU clusters
    Add cpu_lp_mask, cpu_perf_mask, and cpu_prime_mask to represent the CPUs
    that belong to each cluster in a tri-cluster, heterogeneous system.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Tkpointz <pointzztk@gmail.com>
    Signed-off-by: nishant6342 <www.rajsonu13@gmail.com>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -55,6 +55,11 @@ extern unsigned int nr_cpu_ids;
  *     cpu_online_mask  - has bit 'cpu' set iff cpu available to scheduler
  *     cpu_active_mask  - has bit 'cpu' set iff cpu available to migration
  *     cpu_isolated_mask    - has bit 'cpu' set iff cpu isolated
+ *     isolated_mask    - has bit 'cpu' set iff cpu isolated
+ *     cpu_lp_mask      - has bit 'cpu' set iff cpu is part of little cluster
+ *     cpu_perf_mask    - has bit 'cpu' set iff cpu is part of big cluster
+ *     cpu_prime_mask   - has bit 'cpu' set iff cpu is part of prime cluster
+ *
  *  If !CONFIG_HOTPLUG_CPU, present == possible, and active == online.
  *
  *  The cpu_possible_mask is fixed at boot time, as the set of CPU id's
@@ -100,6 +105,10 @@ extern struct cpumask __cpu_isolated_mask;
 #ifdef CONFIG_MTK_SCHED_EXTENSION
 #define cpu_isolated_mask   ((const struct cpumask *)&__cpu_isolated_mask)
 #endif
+extern const struct cpumask *const cpu_lp_mask;
+extern const struct cpumask *const cpu_perf_mask;
+extern const struct cpumask *const cpu_prime_mask;
+
 #if NR_CPUS > 1
 #define num_online_cpus()	cpumask_weight(cpu_online_mask)
 #define num_possible_cpus()	cpumask_weight(cpu_possible_mask)
