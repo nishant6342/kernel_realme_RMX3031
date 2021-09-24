e7e1e7d2c77e4540aa21025bcf1a27816814928f
raymanfx <raymanfx@gmail.com>
2021-09-24 11:13:28 +0000

    kernel: module: Ignore all magic mismatches
    Change-Id: Icc96f9a5c084ab4734a3d982a8f185dbde045f11
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>
    Signed-off-by: neilchetty <neilchetty4559@gmail.com>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Signed-off-by: nishant6342 <www.rajsonu13@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -3761,6 +3761,10 @@ static int load_module(struct load_info *info, const char __user *uargs,
 		goto free_copy;
 	}
 
+	//FIXME
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
+
 	err = module_sig_check(info, flags);
 	if (err)
 		goto free_copy;
