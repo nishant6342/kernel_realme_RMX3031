8ced68c705a12e6af89429053c3388babce6f943
Nishant Kumar <www.rajsonu13@gmail.com>
2022-05-01 16:05:40 +0530

    Fix build errors after merge

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -1444,17 +1444,6 @@ struct uncached_list {
 
 static DEFINE_PER_CPU_ALIGNED(struct uncached_list, rt_uncached_list);
 
-static void rt_add_uncached_list(struct rtable *rt)
-{
-	struct uncached_list *ul = raw_cpu_ptr(&rt_uncached_list);
-
-	rt->rt_uncached_list = ul;
-
-	spin_lock_bh(&ul->lock);
-	list_add_tail(&rt->rt_uncached, &ul->head);
-	spin_unlock_bh(&ul->lock);
-}
-
 static bool rt_cache_route(struct fib_nh *nh, struct rtable *rt)
 {
 	struct rtable *orig, *prev, **p;
@@ -1484,11 +1473,6 @@ static bool rt_cache_route(struct fib_nh *nh, struct rtable *rt)
 
 	return ret;
 }
-
-struct uncached_list {
-	spinlock_t		lock;
-	struct list_head	head;
-};
 
 static DEFINE_PER_CPU_ALIGNED(struct uncached_list, rt_uncached_list);
 
