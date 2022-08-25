caf687dba1e1e80c6a13395c3638e72ff50d5e97
john yan <john.yan@mediatek.com>
2022-08-25 13:53:06 +0800

    [ALPS07361801] disp:display idlemgr thread
    [Detail] it will be flash garbage when enter idle during show logo
    
    [Solution] the idlemgr thread should be work after frame trigger
    
    MTK-Commit-Id: f4bcd3670f6095a4b2aa443ca4868f6a9a9c49cc
    
    CR-Id: ALPS07361801
    Feature: display_mobile
    Change-Id: I74baaeb714a16ed16d507ac0ed34ccab6440974b
    Signed-off-by: john yan <john.yan@mediatek.com>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- drivers/misc/mediatek/video/mt6765/videox/disp_lowpower.c
+++ drivers/misc/mediatek/video/mt6765/videox/disp_lowpower.c
@@ -1143,6 +1143,8 @@ static int _primary_path_idlemgr_monitor_thread(void *data)
 	int ret = 0;
 	unsigned long long interval = 0;
 	unsigned long long time_diff;
+	g_idle_skip = 0;
+	g_idle_skip_trigger = 0;
 
 	msleep(16000);
 	while (1) {
@@ -1166,6 +1168,10 @@ static int _primary_path_idlemgr_monitor_thread(void *data)
 
 		primary_display_manual_lock();
 
+		if (g_idle_skip == 0) {
+			primary_display_manual_unlock();
+			continue;
+		}
 		if (primary_get_state() != DISP_ALIVE) {
 			primary_display_manual_unlock();
 			primary_display_wait_state(DISP_ALIVE,
