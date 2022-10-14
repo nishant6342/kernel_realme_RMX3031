55ba83669d0e74ba800e376988e774bda06fb183
Tejas Udupa <tejasudupa1285@gmail.com>
2022-10-14 19:39:30 +0530

    power_limit: update to v1.3
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- drivers/cpufreq/power_limit.c
+++ drivers/cpufreq/power_limit.c
@@ -2,14 +2,16 @@
  * @file    power_limit.c
  * @author  Tejas udupa
  * @date    12 May 2022
- * 
+ *
+ * @version 1.3 - Add Game Boost functionalities
+ *              This adds game boost threshold, profile and auto throttle
  * @version 1.2 - Fix frequency values being held even after suspend by saving
  *              values in user_freq array struct and checking for freq_held.
  *              apply user_freq values on release.
- *              
+ *
  * @version 1.1 - add policy_apply_limits() function which can be used
  *              to clamp max, min for current policy
- * 
+ *
  * @brief  Frequency control driver, clamps max frequency
  * to profile selected when display is suspended, and
  * releases the clamp when out of suspend. it uses
@@ -25,19 +27,23 @@
 #include <linux/notifier.h>
 #include <linux/fb.h>
 #include <linux/pm_qos.h>
+#include <linux/workqueue.h>
+#include <linux/kthread.h>
 #include "helio-dvfsrc-opp.h"
 #include "mtk_ppm_api.h"
 #include "mtk_perfmgr_internal.h"
 #include "power_limit.h"
 
 static bool init_done = 0;
+/*Creating work by Static Method */
+DECLARE_WORK(workqueue,boost_wq);
 
 void policy_apply_limits(void)
-{    
+{
     struct cpufreq_policy *policy;
     int i;
 
-    if(!init_done || freq_held)
+    if(!init_done || freq_held || !gov_ctl)
         return;
 
     for (i = 0; i < cluster_num; i++) {
@@ -62,7 +68,7 @@ static int freq_hold(void)
         pr_debug("%s: cluster:%d max:%d min:%d saved max:%d\n",__func__, i,
             freq_to_set[i].max, freq_to_set[i].min, user_freq[i].max);
     }
-    
+
     retval = mt_ppm_userlimit_cpu_freq(cluster_num, freq_to_set);
     /* prevent any and all policy limits updates when freq is held or
      * device is in sleep */
@@ -81,7 +87,7 @@ static int freq_release(void)
         pr_debug("%s: cluster:%d max:%d min:%d\n",__func__, i,
             user_freq[i].max, user_freq[i].min);
     }
-    
+
     retval = mt_ppm_userlimit_cpu_freq(cluster_num, freq_to_set);
     freq_held = false;
 
@@ -112,6 +118,129 @@ static int vcorefs_release(void)
 	return 0;
 }
 
+/*
+ * Game Boost functions
+ */
+static int set_freq_arr(unsigned int prof_arr[])
+{
+    int i,retval;
+
+    for (i = 0; i < cluster_num; i++) {
+        freq_to_set[i].min = -1;
+        freq_to_set[i].max = prof_arr[i];
+        pr_debug("%s: cluster:%d max:%d min:%d saved max:%d\n",__func__, i,
+            freq_to_set[i].max, freq_to_set[i].min, user_freq[i].max);
+    }
+
+    retval = mt_ppm_userlimit_cpu_freq(cluster_num, freq_to_set);
+    /* prevent any and all policy limits updates when freq is held or
+     * device is in sleep */
+    freq_held = true;
+
+    return retval;
+}
+
+static int core_hold_boost(void)
+{
+    pm_qos_update_request(&pm_qos_req, 50);
+    return 0;
+}
+
+static int vcorefs_hold_boost(void)
+{
+    pm_qos_update_request(&pm_qos_ddr_req, DDR_OPP_0);
+    return 0;
+}
+
+enum hrtimer_restart uboost_callback(struct hrtimer *timer)
+{
+    core_release();
+    vcorefs_release();
+    set_freq_arr(freq_profiles[perf_mode]);
+    nap_time_ms = 500;
+    return HRTIMER_RESTART;
+}
+
+void boost_wq(struct work_struct *work)
+{
+    unsigned int boost_freq[] = {2000000, 2600000, 3000000};
+    //Boost all frequencies
+    set_freq_arr(boost_freq);
+    core_hold_boost();
+    vcorefs_hold_boost();
+    //Hold boost for fixed duration
+    hrtimer_start(&boosthold_timer, ms_to_ktime(boost_duration),
+        HRTIMER_MODE_REL);
+}
+
+/*
+ * update_load():- gives current load on the respective cpu core.
+ */
+static u64 update_load(int cpu)
+{
+    u64 cur_wall_time;
+    u64 cur_idle_time;
+    int io_busy = 0;
+    unsigned int wall_time, idle_time, load = 0;
+
+    cur_idle_time = get_cpu_idle_time(cpu, &cur_wall_time, io_busy);
+    wall_time = (unsigned int)
+            (cur_wall_time - prev_cpu_wall);
+    idle_time = (unsigned int)
+            (cur_idle_time - prev_cpu_idle);
+
+    prev_cpu_wall = cur_wall_time;
+    prev_cpu_idle = cur_idle_time;
+    load = 100 * (wall_time - idle_time) / wall_time;
+
+    return load;
+}
+
+static int load_chk_thread(void *data)
+{
+    int load, prime_core = 6;
+
+    while(!kthread_should_stop())
+    {
+        set_current_state(TASK_RUNNING);
+        if(!run_state){
+            set_current_state(TASK_INTERRUPTIBLE);
+            schedule();
+        }
+        load = update_load(prime_core);
+        if(load >= boost_threshold){
+            schedule_work(&workqueue);
+            nap_time_ms = boost_duration * 2;
+        }
+
+        set_current_state(TASK_INTERRUPTIBLE);
+        schedule_timeout(msecs_to_jiffies(nap_time_ms));
+    }
+    return 0;
+}
+
+static void set_mode(int profile_mode)
+{
+    switch(profile_mode)
+    {
+        case POWER_MODE:
+        case BALANCE_MODE:
+                set_freq_arr(freq_profiles[profile_mode]);
+                gov_ctl = 0;    //disable governor control
+                run_state = 0;  //disable load check
+                break;
+        case PERF_MODE:
+                set_freq_arr(freq_profiles[profile_mode]);
+                gov_ctl = 0;
+                run_state = 1;
+                wake_up_process(lc_thread);
+                break;
+        case 3:
+                run_state = 0;
+                gov_ctl = 1;
+        default:break;
+    }
+}
 
 static int fb_action(struct notifier_block *self,
     unsigned long event, void *data)
@@ -133,7 +262,7 @@ static int fb_action(struct notifier_block *self,
             break;
         /* LCM OFF */
         case FB_BLANK_POWERDOWN:
-            if(enable){    
+            if(enable){
                 freq_hold();
                 core_hold();
                 vcorefs_hold();
@@ -181,10 +310,67 @@ static ssize_t enable_store(struct kobject *kobj, struct kobj_attribute *attr,
     return count;
 }
 
+static ssize_t gov_ctl_show(struct kobject *kobj, struct kobj_attribute *attr,
+                  char *buf)
+{
+    return sprintf(buf,"governor cpufreq control:%d\n", gov_ctl);
+}
+static ssize_t gov_ctl_store(struct kobject *kobj, struct kobj_attribute *attr,
+                  const char *buf, size_t count)
+{
+    sscanf(buf,"%d", &perf_mode);
+    return count;
+}
+static ssize_t mode_show(struct kobject *kobj, struct kobj_attribute *attr,
+                  char *buf)
+{
+    char* arr[] = {"Power Mode",
+                    "Balance Mode",
+                    "Performance Mode", "Manual Mode"};
+    return sprintf(buf,"%s\n", arr[perf_mode]);
+}
+static ssize_t mode_store(struct kobject *kobj, struct kobj_attribute *attr,
+                  const char *buf, size_t count)
+{
+    sscanf(buf,"%d", &perf_mode);
+    set_mode(perf_mode);
+    return count;
+}
+static ssize_t bst_time_show(struct kobject *kobj, struct kobj_attribute *attr,
+                  char *buf)
+{
+    return sprintf(buf,"%d\n", boost_duration);
+}
+static ssize_t bst_time_store(struct kobject *kobj, struct kobj_attribute *attr,
+                  const char *buf, size_t count)
+{
+    sscanf(buf,"%d", &boost_duration);
+    return count;
+}
+static ssize_t bst_thresh_show(struct kobject *kobj, struct kobj_attribute *attr,
+                  char *buf)
+{
+    return sprintf(buf,"%d\n", boost_threshold);
+}
+static ssize_t bst_thresh_store(struct kobject *kobj, struct kobj_attribute *attr,
+                  const char *buf, size_t count)
+{
+    sscanf(buf,"%d", &boost_threshold);
+    return count;
+}
+
 static struct kobj_attribute pwr_mode =
     __ATTR(power_profile, 0644, pwr_prof_show, pwr_prof_store);
 static struct kobj_attribute enable_limiter =
     __ATTR(enable, 0644, enable_show, enable_store);
+static struct kobj_attribute enable_gov_ctl =
+    __ATTR(governor_ctl, 0644, gov_ctl_show, gov_ctl_store);
+static struct kobj_attribute profile_mode =
+    __ATTR(profile_mode, 0644, mode_show, mode_store);
+static struct kobj_attribute bst_time =
+    __ATTR(boost_duration, 0644, bst_time_show, bst_time_store);
+static struct kobj_attribute bst_thresh =
+    __ATTR(boost_threshold, 0644, bst_thresh_show, bst_thresh_store);
 
 /*********************************** sysfs end ***********************************/
 
@@ -192,7 +378,7 @@ static int __init freq_init(void)
 {
     struct cpufreq_policy *policy;
     int rc = 0, i;
-    
+
     cluster_num = arch_get_nr_clusters();
 
     freq_to_set = kcalloc(cluster_num,
@@ -240,13 +426,48 @@ static int __init freq_init(void)
         rc = -1;
         goto out;
     }
+    rc = sysfs_create_file(power_limiter_kobj, &enable_gov_ctl.attr);
+    if (rc) {
+        pr_err(KERN_WARNING "%s: sysfs_create_file failed for power_limiter\n", __func__);
+        rc = -1;
+        goto out;
+    }
+    rc = sysfs_create_file(power_limiter_kobj, &profile_mode.attr);
+    if (rc) {
+        pr_err(KERN_WARNING "%s: sysfs_create_file failed for power_limiter\n", __func__);
+        rc = -1;
+        goto out;
+    }
+    rc = sysfs_create_file(power_limiter_kobj, &bst_time.attr);
+    if (rc) {
+        pr_err(KERN_WARNING "%s: sysfs_create_file failed for power_limiter\n", __func__);
+        rc = -1;
+        goto out;
+    }
+    rc = sysfs_create_file(power_limiter_kobj, &bst_thresh.attr);
+    if (rc) {
+        pr_err(KERN_WARNING "%s: sysfs_create_file failed for power_limiter\n", __func__);
+        rc = -1;
+        goto out;
+    }
+
+    lc_thread = kthread_create(load_chk_thread,NULL,"loadchk thread");
+    if(!lc_thread)
+        pr_debug(KERN_ERR "%s:failed to create kthread\n",__func__);
+
+    hrtimer_init(&boosthold_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+    boosthold_timer.function = &uboost_callback;
+
     pm_qos_add_request(&pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
 		PM_QOS_DEFAULT_VALUE);
 
     pm_qos_add_request(&pm_qos_ddr_req, PM_QOS_DDR_OPP,
 		PM_QOS_DDR_OPP_DEFAULT_VALUE);
+
+    //set flags
     enable = 1;
     init_done = 1;
+    gov_ctl = 1;
 out:
     return rc;
 }
--- drivers/cpufreq/power_limit.h
+++ drivers/cpufreq/power_limit.h
@@ -1,20 +1,42 @@
+#ifndef _POWER_LIMIT_H_
+#define _POWER_LIMIT_H_
+
 #define LIMITER_OFF 3
 #define LIMITER_H 2
 #define LIMITER_M 1
 #define LIMITER_L 0
+#define POWER_MODE 0 
+#define BALANCE_MODE 1
+#define PERF_MODE 2
 
 static struct kobject *power_limiter_kobj;
 static struct ppm_limit_data *freq_to_set;
 static struct ppm_limit_data *user_freq;
 static struct pm_qos_request pm_qos_req;
 static struct pm_qos_request pm_qos_ddr_req;
-static int cluster_num;
+//flags
 static bool enable;
+static bool gov_ctl;
+static bool run_state = 0;
 static bool freq_held = false;
+
+static int cluster_num;
+static int perf_mode = 1;
+static int prev_cpu_wall = 0, prev_cpu_idle = 0;
+static int boost_threshold = 90, boost_duration = 2000;
+static unsigned int nap_time_ms = 500;
 static int cpu_num[3] = {0, 4, 7};
 static int power_profile = 1; //Power saver profile 0-Low 1-Mid 2-High 3-Off
 static unsigned int freq_to_hold[3][3] = {  {1725000, 1350000, 1075000}, //Little
                                             {1537000, 1335000, 902000}, //Big
                                             {1998000, 1258000, 659000}}; //Prime
+static unsigned int freq_profiles[3][3] = { {1525000, 1451000, 659000},
+                                            {2000000, 2200000, 1632000},
+                                            {2000000, 2507000, 2463000}};
                                             
 extern void policy_apply_limits(void);
+static struct task_struct *lc_thread, *timer_thread;
+void boost_wq(struct work_struct *work);
+static struct hrtimer boosthold_timer;
+
+#endif
