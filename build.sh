74ea2c166e4af13a8ad159a5c2f491ddb62a802c
Nishant Kumar <www.rajsonu13@gmail.com>
2023-09-20 08:22:58 +0000

    build.sh Don't sleep

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ function compile()
 {
 echo ORIGIN KERNEL FOR REALME X7 MAX 5G, CODENAMED - CUPIDA
 echo
-sleep 3 >/dev/null
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G >/dev/null
@@ -23,18 +22,15 @@ fi
 read -p "Wanna do dirty build? (Y/N): " build_type
 if [[ $build_type == "N" || $build_type == "n" ]]; then
 echo Deleting out directory and doing clean Build
-sleep 3 >/dev/null
 rm -rf out && mkdir -p out
 fi
 if [[ $build_type == "Y" || $build_type == "y" ]]; then
 echo Warning :- Doing dirty build
-sleep 3 >/dev/null
 fi
 if ! [[ $build_type == "Y" || $build_type == "y" ]]; then
 if ! [[ $build_type == "N" || $build_type == "n" ]]; then
 echo Invalid Input , Read carefully before typing
 echo Trying to restart script
-sleep 3 >/dev/null
 . build.sh && exit
 fi
 fi
